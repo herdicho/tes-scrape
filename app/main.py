3df97fbca98344559c64d6652205e8e05ba2ed27
Farras Herdicho <farras.herdicho@mekari.com>
2024-05-19 22:52:22 +0700

    fix code

--- app/main.py
+++ app/main.py
@@ -1,14 +1,7 @@
 from fastapi import FastAPI, Response
 from pydantic import BaseModel
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from langchain_community.document_loaders import SeleniumURLLoader
 import json
-import textwrap
-from fpdf import FPDF
-from PyPDF2 import PdfFileMerger
 
 class Url(BaseModel):
   url: str
@@ -17,17 +10,6 @@ app = FastAPI()
 
 @app.post("/scrape_url")
 def scrape_url(url: Url):
-  # Set up Chrome options
-  chrome_options = webdriver.ChromeOptions()
-  chrome_options.add_argument("--headless")
-  chrome_options.add_argument("--no-sandbox")
-  chrome_options.add_argument("--disable-dev-shm-usage")
-  
-  service = Service(ChromeDriverManager().install())
-
-  # Set path to chromedriver as per Docker setup
-  driver = webdriver.Chrome(service=service, options=chrome_options)
-  
   # Use the SeleniumURLLoader with the provided driver
   loader = SeleniumURLLoader(urls=[url.url], browser='chrome', arguments=[
           "--disable-dev-shm-usage",
@@ -41,9 +23,6 @@ def scrape_url(url: Url):
   # Get the data
   content = loader.load()
   
-  # Close the driver
-  driver.quit()
-
   result = [
     { "Content": content[0].page_content }
   ]
